1ab806002572e43f238cd39fb4e97a1022dccb64
Ben Hall <benji2357@gmail.com>
2018-07-27 08:03:01 +0100

    fix(build): attempt to not have to bundle definition files

--- scripts/removeCssImports.js
+++ scripts/removeCssImports.js
@@ -1,11 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 
-const distPath = path.join(__dirname, '..', 'dist')
+const recursiveMapOverDefinitionFiles = (f, pathToCheck) => {
+  for (const name of fs.readdirSync(pathToCheck)) {
+    const nextPath = path.join(pathToCheck, name)
 
-const definition = fs.readFileSync(path.join(distPath, 'eri.d.ts'), 'utf-8')
+    if (fs.lstatSync(nextPath).isDirectory()) {
+      recursiveMapOverDefinitionFiles(f, nextPath)
+    } else if (nextPath.endsWith('.d.ts')) {
+      fs.writeFileSync(nextPath, f(fs.readFileSync(nextPath, 'utf-8')))
+    }
+  }
+}
+
+const removeCssImports = definition => definition
   .split('\n')
   .filter(line => !(line.trim().startsWith('import') && line.endsWith(".css';")))
   .join('\n')
 
-fs.writeFileSync(path.join(distPath, 'index.d.ts'), definition)
+recursiveMapOverDefinitionFiles(removeCssImports, path.join(__dirname, '..', 'dist'))
