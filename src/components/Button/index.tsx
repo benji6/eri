86e054a038f35918410bd0ebcce1f3da52a88e5d
Ben Hall <benji2357@gmail.com>
2018-08-05 21:29:11 +0100

    feat(Button): disabled variation no longer renders a spinner

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,11 @@
 import * as React from 'react'
-import Spinner from '../Spinner'
 import './style.css'
 
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 class Button extends React.PureComponent<IProps> {
   render() {
-    const { children, disabled, type = 'submit', ...rest } = this.props
-
-    return (
-      <button
-        {...rest}
-        className="e-button e-button--primary"
-        disabled={disabled}
-        type={type}
-      >
-        {disabled ? <Spinner variation="ground" /> : children}
-      </button>
-    )
+    return <button {...this.props} className="e-button e-button--primary" />
   }
 }
 
