418a60e65f2a728df534a4080ae2895fb8dd90dc
Ben Hall <benji2357@gmail.com>
2018-08-05 09:59:56 +0100

    docs(typography): add some lorem ipsum text

--- docs/index.tsx
+++ docs/index.tsx
@@ -150,8 +150,31 @@ class App extends React.PureComponent {
           <h3>Examples</h3>
           <h1>Heading 1</h1>
           <h2>Heading 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
           <h3>Heading 3</h3>
-          <p>Paragraph</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
           <h3>Centering text</h3>
           <p e-util="center">
             You can center any text by setting the e-util attribute to "center"
