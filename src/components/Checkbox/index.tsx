60a0dd24b57086a55441334ad7f27e819c4cbd78
Ben Hall <benji2357@gmail.com>
2018-09-03 00:19:28 +0100

    feat(Checkbox): use check Icon instead of unicode character

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import FieldError from '../../privateComponents/FieldError'
 import './style.css'
+import Icon from '../Icon'
 
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
   error?: React.ReactNode
@@ -20,7 +21,9 @@ export default class Checkbox extends React.PureComponent<IProps> {
             className="e-checkbox__input"
             type="checkbox"
           />
-          <span className="e-checkbox__appearance">✓</span>
+          <span className="e-checkbox__appearance">
+            <Icon name="check" />
+          </span>
           <span className="e-checkbox__label-copy">{label}</span>
         </label>
         <FieldError>{error}</FieldError>
