e9af59b184d78711e7c421bcaef2be779b9ba13b
Ben Hall <benji2357@gmail.com>
2018-10-06 15:01:47 +0100

    fix(modal): attempt to fix scroll bug

--- src/privateComponents/Mask/index.tsx
+++ src/privateComponents/Mask/index.tsx
@@ -2,8 +2,8 @@ import classnames from 'classnames'
 import * as React from 'react'
 import './style.css'
 
-const documentElementClassList = (document.documentElement as HTMLElement)
-  .classList
+const documentElement = document.documentElement as HTMLElement
+const documentElementClassList = documentElement.classList
 
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   open: boolean
@@ -11,23 +11,17 @@ interface IProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export default class Mask extends React.PureComponent<IProps> {
+  public scrollY: number = 0
+
   public componentDidMount() {
-    if (this.props.open) {
-      documentElementClassList.add('e-no-scroll')
-    }
+    if (this.props.open) this.openMask()
     window.addEventListener('keydown', this.handleKeyDown)
   }
 
   public componentWillReceiveProps(nextProps: IProps) {
-    if (this.props.open === nextProps.open) {
-      return
-    }
-
-    if (nextProps.open) {
-      documentElementClassList.add('e-no-scroll')
-    } else {
-      documentElementClassList.remove('e-no-scroll')
-    }
+    if (this.props.open === nextProps.open) return
+    if (nextProps.open) this.openMask()
+    else this.closeMask()
   }
 
   public componentWillUnmount() {
@@ -46,11 +40,25 @@ export default class Mask extends React.PureComponent<IProps> {
       <div
         aria-hidden={!open}
         className={className}
+        onClick={onClose}
         onKeyDown={this.handleKeyDown}
       >
-        <div {...rest} className="e-mask__container" onClick={onClose} />
+        <div {...rest} />
       </div>
     )
+  }
+
+  private closeMask() {
+    documentElementClassList.remove('e-no-scroll')
+    window.scrollTo(0, this.scrollY)
+    documentElement.style.top = null
+  }
+
+  private openMask() {
+    const { scrollY } = window
+    this.scrollY = scrollY
+    documentElement.style.top = `${-scrollY}px`
+    documentElementClassList.add('e-no-scroll')
   }
 
   private handleKeyDown = (
