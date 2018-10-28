ca44099e98244c006eb1361efe71007eac8b1742
Ben Hall <benji2357@gmail.com>
2018-10-28 21:14:23 +0000

    fix(MenuButton): fix component name and add aria-label

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -4,10 +4,10 @@ import './style.css'
 
 interface IProps extends React.HtmlHTMLAttributes<HTMLButtonElement> {}
 
-export default class MenuList extends React.PureComponent<IProps> {
+export default class MenuButton extends React.PureComponent<IProps> {
   public render() {
     return (
-      <button {...this.props} className="e-menu-button">
+      <button {...this.props} aria-label="open menu" className="e-menu-button">
         <Icon name="menu" size="3" />
       </button>
     )
