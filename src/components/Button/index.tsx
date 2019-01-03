fb3ef222d40123abbc23cb07fde62f45e0a6b280
Ben Hall <benji2357@gmail.com>
2019-01-03 21:29:56 +0000

    refactor(Button): change from class component to function component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,18 +7,15 @@ interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary'
 }
 
-export default class Button extends React.Component<IProps> {
-  public render() {
-    const {
-      sentiment,
-      variant = 'primary',
-      type = 'submit', // Formik gets grumpy if you don't specify this
-      ...rest
-    } = this.props
-    const className = classnames(`e-button e-button--${variant}`, {
-      'e-button--negative': sentiment === 'negative',
-    })
+export default function Button({
+  sentiment,
+  variant = 'primary',
+  type = 'submit', // Formik gets grumpy if you don't specify this
+  ...rest
+}: IProps) {
+  const className = classnames(`e-button e-button--${variant}`, {
+    'e-button--negative': sentiment === 'negative',
+  })
 
-    return <button {...rest} className={className} type={type} />
-  }
+  return <button {...rest} className={className} type={type} />
 }
