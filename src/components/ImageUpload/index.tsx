090fa6b83960602ff1055d9191c740f901297e05
Ben Hall <benji2357@gmail.com>
2019-03-01 19:29:43 +0000

    refactor(ImageUpload): refactor to use hooks

--- src/components/ImageUpload/index.tsx
+++ src/components/ImageUpload/index.tsx
@@ -15,53 +15,29 @@ interface IState {
   imageUrl?: string
 }
 
-export default class ImageUpload extends React.PureComponent<IProps> {
-  public state: IState = {
+const handleLoad = ({ target: { src } }: React.ChangeEvent<HTMLImageElement>) =>
+  URL.revokeObjectURL(src)
+
+export default function ImageUpload({
+  error,
+  label,
+  onChange,
+  ...rest
+}: IProps) {
+  const [{ imageName, imageUrl }, setState] = React.useState<IState>({
     imageName: undefined,
     imageUrl: undefined,
-  }
+  })
 
-  public render() {
-    const { error, label, ...rest } = this.props
-    const { imageName, imageUrl } = this.state
-
-    return (
-      <Field>
-        <label className="e-image-upload__label">
-          <FieldLabel>{label}</FieldLabel>
-          {imageUrl && (
-            <img
-              alt={`preview of ${imageName}`}
-              className="e-image-upload__img"
-              onLoad={this.handleLoad}
-              src={imageUrl}
-            />
-          )}
-          <input
-            required
-            {...rest}
-            accept="image/*"
-            aria-invalid={Boolean(error)}
-            className="e-image-upload__input"
-            onChange={this.handleChange}
-            type="file"
-          />
-        </label>
-        <FieldError>{error}</FieldError>
-      </Field>
-    )
-  }
-
-  private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files as FileList
-    const { onChange } = this.props
 
     if (!files.length) {
-      this.setState({ imageName: undefined, imageUrl: undefined })
+      setState({ imageName: undefined, imageUrl: undefined })
     } else {
       const file = files[0]
 
-      this.setState({
+      setState({
         imageName: file.name,
         imageUrl: URL.createObjectURL(file),
       })
@@ -70,7 +46,29 @@ export default class ImageUpload extends React.PureComponent<IProps> {
     if (onChange) onChange(e)
   }
 
-  private handleLoad = ({
-    target: { src },
-  }: React.ChangeEvent<HTMLImageElement>) => URL.revokeObjectURL(src)
+  return (
+    <Field>
+      <label className="e-image-upload__label">
+        <FieldLabel>{label}</FieldLabel>
+        {imageUrl && (
+          <img
+            alt={`preview of ${imageName}`}
+            className="e-image-upload__img"
+            onLoad={handleLoad}
+            src={imageUrl}
+          />
+        )}
+        <input
+          required
+          {...rest}
+          accept="image/*"
+          aria-invalid={Boolean(error)}
+          className="e-image-upload__input"
+          onChange={handleChange}
+          type="file"
+        />
+      </label>
+      <FieldError>{error}</FieldError>
+    </Field>
+  )
 }
