9ec61500a3b54140e4548af5321b5601f8fedcf2
Ben Hall <benji2357@gmail.com>
2019-03-10 12:35:43 +0000

    feat(Card): becomes a button when passed onClick prop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
 import './style.css'
 
-interface IProps extends React.HtmlHTMLAttributes<HTMLDivElement> {}
+interface IProps
+  extends React.HtmlHTMLAttributes<HTMLButtonElement | HTMLDivElement> {}
 
 export default function Card(props: IProps) {
-  return <div {...props} className="e-card" />
+  if (!props.onClick) return <div {...props} className="e-card" />
+  return <button {...props} className="e-card" />
 }
