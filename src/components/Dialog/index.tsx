4c50d312c74ec8ee1eb84c76c50cb7909757b5bf
Ben Hall <benji2357@gmail.com>
2019-05-26 21:05:58 +0100

    refactor(Dialog): use react-transition-group

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,7 +1,8 @@
-import classnames from 'classnames'
 import * as React from 'react'
+import { CSSTransition } from 'react-transition-group'
 import CloseButton from '../../privateComponents/CloseButton'
 import Mask from '../../privateComponents/Mask'
+import { getCssTime1 } from '../../utils/getCssVar'
 import './style.css'
 
 interface IProps {
@@ -20,30 +21,37 @@ export default function Dialog({
   title,
   ...rest
 }: IProps) {
-  const className = classnames('e-dialog', {
-    'e-dialog--closed': !open,
-  })
-
   return (
     <Mask onClose={disableClose ? undefined : onClose} open={open}>
-      <div
-        {...rest}
-        aria-describedby="e-dialog-desc"
-        aria-labelledby="e-dialog-title"
-        className={className}
-        onClick={disableClose ? undefined : onClose}
-        role="dialog"
+      <CSSTransition
+        classNames="e-dialog-"
+        in={open}
+        mountOnEnter
+        timeout={{ exit: getCssTime1() }}
+        unmountOnExit
       >
-        <div className="e-dialog__dialog" onClick={e => e.stopPropagation()}>
-          <div className="e-dialog__content">
-            <div className="e-dialog__header">
-              <h4 id="e-dialog-title">{title}</h4>
-              <CloseButton disabled={disableClose || !open} onClick={onClose} />
+        <div
+          {...rest}
+          aria-describedby="e-dialog-desc"
+          aria-labelledby="e-dialog-title"
+          className="e-dialog"
+          onClick={disableClose ? undefined : onClose}
+          role="dialog"
+        >
+          <div className="e-dialog__dialog" onClick={e => e.stopPropagation()}>
+            <div className="e-dialog__content">
+              <div className="e-dialog__header">
+                <h4 id="e-dialog-title">{title}</h4>
+                <CloseButton
+                  disabled={disableClose || !open}
+                  onClick={onClose}
+                />
+              </div>
+              <div id="e-dialog-desc">{children}</div>
             </div>
-            <div id="e-dialog-desc">{children}</div>
           </div>
         </div>
-      </div>
+      </CSSTransition>
     </Mask>
   )
 }
