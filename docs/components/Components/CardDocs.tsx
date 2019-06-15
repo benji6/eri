7c9d52538e47ec440e4b8386bb3d49f5fac31f86
Ben Hall <benji2357@gmail.com>
2019-06-15 18:36:28 +0100

    docs(Card): less copy in the cards

--- docs/components/Components/CardDocs.tsx
+++ docs/components/Components/CardDocs.tsx
@@ -8,59 +8,47 @@ export default function CardDocs(_: RouteComponentProps) {
       <h3>Card</h3>
       <p>Wrap your Cards in a CardGroup.</p>
       <CardGroup>
-        <Card onClick={() => undefined}>
+        <Card>
           <h3>Card one</h3>
-          <p>This is card one.</p>
-          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card onClick={() => undefined}>
+        <Card>
           <h3>Card two</h3>
-          <p>This is card two.</p>
-          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card onClick={() => undefined}>
+        <Card>
           <h3>Card three</h3>
-          <p>This is card three.</p>
-          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card onClick={() => undefined}>
+        <Card>
           <h3>Card four</h3>
-          <p>This is card four.</p>
-          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card onClick={() => undefined}>
+        <Card>
           <h3>Card five</h3>
-          <p>This is card five.</p>
+        </Card>
+        <Card>
+          <h3>Card six</h3>
+        </Card>
+        <Card onClick={() => undefined}>
+          <h3>Card seven</h3>
           <p>A card can also be a button like this one.</p>
         </Card>
         <Card onClick={() => undefined}>
-          <h3>Card six</h3>
-          <p>This is card six.</p>
+          <h3>Card eight</h3>
           <p>A card can also be a button like this one.</p>
         </Card>
-        <Card>
-          <h3>Card seven</h3>
-          <p>This is card seven.</p>
-        </Card>
-        <Card>
-          <h3>Card eight</h3>
-          <p>This is card eight.</p>
-        </Card>
-        <Card>
+        <Card onClick={() => undefined}>
           <h3>Card nine</h3>
-          <p>This is card nine.</p>
+          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card>
+        <Card onClick={() => undefined}>
           <h3>Card ten</h3>
-          <p>This is card ten.</p>
+          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card>
+        <Card onClick={() => undefined}>
           <h3>Card eleven</h3>
-          <p>This is card eleven.</p>
+          <p>A card can also be a button like this one.</p>
         </Card>
-        <Card>
+        <Card onClick={() => undefined}>
           <h3>Card twelve</h3>
-          <p>This is card twelve.</p>
+          <p>A card can also be a button like this one.</p>
         </Card>
       </CardGroup>
     </>
