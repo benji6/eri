f4e16b49f996bb655553be9e80e11d0c565291b4
Ben Hall <benji2357@gmail.com>
2019-06-18 19:34:51 +0100

    docs(icon): add favicon

---
+++ scripts/icons.js
@@ -0,0 +1,31 @@
+const favicons = require('favicons')
+const fs = require('fs')
+const path = require('path')
+
+const iconsPath = path.join(__dirname, '..', 'docs', 'icons')
+const source = path.join(iconsPath, 'icon.svg')
+
+const configuration = {
+  online: false,
+  preferOnline: false,
+  icons: {
+    android: false,
+    appleIcon: false,
+    appleStartup: false,
+    coast: false,
+    favicons: true,
+    firefox: false,
+    windows: false,
+    yandex: false,
+  },
+}
+
+favicons(source, configuration, (err, response) => {
+  if (err) throw err
+  const { contents, name } = response.images.find(
+    ({ name }) => name === 'favicon.ico',
+  )
+  fs.writeFile(path.join(iconsPath, name), contents, err => {
+    if (err) throw err
+  })
+})
