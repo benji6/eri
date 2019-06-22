e2f73e6e52678aefabf49bce3f3b350ed4dc0e58
Ben Hall <benji2357@gmail.com>
2019-06-22 22:49:47 +0100

    fix(header): more sane implementation with a div container

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,5 +3,9 @@ import * as React from 'react'
 interface IProps extends React.HtmlHTMLAttributes<HTMLDivElement> {}
 
 export default function Header(props: IProps) {
-  return <header {...props} />
+  return (
+    <header>
+      <div {...props} />
+    </header>
+  )
 }
