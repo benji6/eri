340002d22a14dfc14ab4976b4d5b9017e28a8165
Ben Hall <benji2357@gmail.com>
2019-06-22 17:37:28 +0100

    feat(Header): simplify CSS and markup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react'
-import './style.css'
 
 interface IProps extends React.HtmlHTMLAttributes<HTMLDivElement> {}
 
 export default function Header(props: IProps) {
-  return (
-    <header>
-      <div {...props} className="e-header__container" />
-    </header>
-  )
+  return <header {...props} />
 }
