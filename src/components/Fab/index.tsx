89fb3b5f32a1df5fdaca134f52f32193a41ea26c
Ben Hall <benji2357@gmail.com>
2019-07-06 08:58:28 +0100

    fix(fab): render into portal

--- src/components/Fab/index.tsx
+++ src/components/Fab/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import { CSSTransition } from 'react-transition-group'
 import { getCssTime0 } from '../../utils/getCssVar'
 import './style.css'
+
+const portalEl = document.body.appendChild(document.createElement('div'))
 
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   hide?: boolean
@@ -12,7 +15,7 @@ export default function Fab({
   type = 'submit', // Formik gets grumpy if you don't specify this
   ...rest
 }: IProps) {
-  return (
+  return ReactDOM.createPortal(
     <CSSTransition
       classNames="e-fab-"
       in={!hide}
@@ -21,6 +24,7 @@ export default function Fab({
       unmountOnExit
     >
       <button {...rest} className="e-fab" type={type} />
-    </CSSTransition>
+    </CSSTransition>,
+    portalEl,
   )
 }
