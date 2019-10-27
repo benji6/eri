1263d230759fae5d436cb42032384b6fb0c9c299
Ben Hall <benji2357@gmail.com>
2019-10-27 20:29:10 +0000

    docs(SignInPage): remove artificial wait on submit

--- docs/components/Components/SignInPageDocs.tsx
+++ docs/components/Components/SignInPageDocs.tsx
@@ -5,10 +5,9 @@ import { SignInPage } from '../../../src'
 export default function SignInPageDocs(_: RouteComponentProps) {
   return (
     <SignInPage
-      onSubmit={async ({ email, password }) => {
-        await new Promise(resolve => setTimeout(resolve, 1000))
+      onSubmit={async ({ email, password }) =>
         console.table({ email, password })
-      }}
+      }
       signUpLink={<Link to="/pages/sign-up">Sign up</Link>}
     />
   )
