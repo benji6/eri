f8ee5decef2174b71dc0f4d94cb747e9cd5fccbf
Ben Hall <benji2357@gmail.com>
2019-11-30 15:45:51 +0000

    feat(MenuButton): add "Menu" text to make it very clear the component is a menu button

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react'
 import Icon from '../Icon'
+import './style.css'
 
 export default function MenuButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>,
 ) {
   return (
-    <button
-      {...props}
-      aria-label="open menu"
-      className="e-menu-button"
-      e-util="ripple"
-    >
+    <button {...props} className="e-menu-button" e-util="ripple">
+      <span className="e-menu-button__text">Menu</span>
       <Icon name="menu" size="3" />
     </button>
   )
