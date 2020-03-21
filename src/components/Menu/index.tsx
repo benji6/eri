6edc562d0590bf54e32832182047859134cbfe37
Ben Hall <benji2357@gmail.com>
2020-03-21 17:09:51 +0000

    feat(menu): always open on desktop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,6 +4,7 @@ import { CSSTransition } from 'react-transition-group'
 import CloseButton from '../../privateComponents/CloseButton'
 import Mask from '../../privateComponents/Mask'
 import { getCssTime1 } from '../../utils/getCssVar'
+import useWidth from '../../hooks/useWidth'
 
 interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   open: boolean
@@ -11,20 +12,24 @@ interface IProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export default function Menu({ children, onClose, open, ...rest }: IProps) {
+  const isWideResolution = useWidth() >= 1024
+
   return (
-    <Mask onClose={onClose} open={open}>
+    <Mask onClose={onClose} open={open && !isWideResolution}>
       <CSSTransition
         classNames="e-menu-"
-        in={open}
+        in={isWideResolution || open}
         mountOnEnter
         timeout={{ exit: getCssTime1() + 100 }}
         unmountOnExit
       >
         {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
         <nav {...rest} className="e-menu" onClick={e => e.stopPropagation()}>
-          <div className="e-menu__close-button">
-            <CloseButton onClick={onClose} />
-          </div>
+          {!isWideResolution && (
+            <div className="e-menu__close-button">
+              <CloseButton onClick={onClose} />
+            </div>
+          )}
           {children}
         </nav>
       </CSSTransition>
