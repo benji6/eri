69f001f820ba14e0e479029c5d378d3da69b9cab
Ben Hall <benji2357@gmail.com>
2020-03-22 19:51:07 +0000

    refactor(menu): simplify components rendered on desktop

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -14,22 +14,25 @@ interface IProps extends React.HTMLAttributes<HTMLDivElement> {
 export default function Menu({ children, onClose, open, ...rest }: IProps) {
   const isWideResolution = useWidth() >= 1024;
 
-  return (
-    <Mask onClose={onClose} open={open && !isWideResolution}>
+  return isWideResolution ? (
+    /* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
+    <nav {...rest} className="e-menu" onClick={(e) => e.stopPropagation()}>
+      {children}
+    </nav>
+  ) : (
+    <Mask onClose={onClose} open={open}>
       <CSSTransition
         classNames="e-menu-"
-        in={isWideResolution || open}
+        in={open}
         mountOnEnter
         timeout={{ exit: getCssTime1() + 100 }}
         unmountOnExit
       >
         {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
         <nav {...rest} className="e-menu" onClick={(e) => e.stopPropagation()}>
-          {!isWideResolution && (
-            <div className="e-menu__close-button">
-              <CloseButton onClick={onClose} />
-            </div>
-          )}
+          <div className="e-menu__close-button">
+            <CloseButton onClick={onClose} />
+          </div>
           {children}
         </nav>
       </CSSTransition>
