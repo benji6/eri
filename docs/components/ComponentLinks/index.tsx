2a9b9bb2e84fadee90e2e13bf2f922f4a90a83c7
Ben Hall <benji2357@gmail.com>
2020-03-28 12:39:41 +0000

    docs(links): remove unnecessary nav elements

--- docs/components/ComponentLinks/index.tsx
+++ docs/components/ComponentLinks/index.tsx
@@ -5,74 +5,72 @@ export default function ComponentLinks(
   props: RouteComponentProps & React.HTMLAttributes<HTMLAnchorElement>
 ) {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link {...props} to="/components/button">
-            Button
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/card">
-            Card
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/dialog">
-            Dialog
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/fab">
-            Fab
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/form">
-            Form
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/header">
-            Header
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/icon">
-            Icon
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/link">
-            Link
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/main">
-            Main
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/pagination">
-            Pagination
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/paper">
-            Paper
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/share-button">
-            ShareButton
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/components/spinner">
-            Spinner
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <ul>
+      <li>
+        <Link {...props} to="/components/button">
+          Button
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/card">
+          Card
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/dialog">
+          Dialog
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/fab">
+          Fab
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/form">
+          Form
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/header">
+          Header
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/icon">
+          Icon
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/link">
+          Link
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/main">
+          Main
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/pagination">
+          Pagination
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/paper">
+          Paper
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/share-button">
+          ShareButton
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/components/spinner">
+          Spinner
+        </Link>
+      </li>
+    </ul>
   );
 }
--- docs/components/PageLinks/index.tsx
+++ docs/components/PageLinks/index.tsx
@@ -5,29 +5,27 @@ export default function PageLinks(
   props: RouteComponentProps & React.HTMLAttributes<HTMLAnchorElement>
 ) {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link {...props} to="/pages/resend-verification">
-            Resend Verification
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/pages/sign-in">
-            Sign in
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/pages/sign-up">
-            Sign up
-          </Link>
-        </li>
-        <li>
-          <Link {...props} to="/pages/verify">
-            Verify
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <ul>
+      <li>
+        <Link {...props} to="/pages/resend-verification">
+          Resend Verification
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/pages/sign-in">
+          Sign in
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/pages/sign-up">
+          Sign up
+        </Link>
+      </li>
+      <li>
+        <Link {...props} to="/pages/verify">
+          Verify
+        </Link>
+      </li>
+    </ul>
   );
 }
