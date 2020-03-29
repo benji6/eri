1221dea776de5194b6422dce3487f1c3ef87ddc5
Ben Hall <benji2357@gmail.com>
2020-03-29 15:37:05 +0100

    feat(Spinner): add inline variation

--- docs/components/Components/SpinnerDocs.tsx
+++ docs/components/Components/SpinnerDocs.tsx
@@ -1,15 +1,36 @@
 import * as React from "react";
+import ConfigurableExample, {
+  PropControlBoolean,
+} from "./_ConfigurableExample";
 import { Paper, Spinner } from "../../../src";
-import ConfigurableExample from "./_ConfigurableExample";
+import { IProps } from "../../../src/components/Spinner";
 import { RouteComponentProps } from "@reach/router";
 
 export default function SpinnerDocs(_: RouteComponentProps) {
+  const [exampleProps, setExampleProps] = React.useState<IProps>({
+    inline: false,
+  });
+  const spinnerEl = <Spinner {...exampleProps} />;
   return (
     <Paper.Group>
       <Paper>
         <h2>Spinner</h2>
       </Paper>
-      <ConfigurableExample example={<Spinner />} />
+      <ConfigurableExample
+        example={
+          exampleProps.inline ? (
+            <p e-util="center">An inline example {spinnerEl}</p>
+          ) : (
+            spinnerEl
+          )
+        }
+      >
+        <PropControlBoolean
+          name="inline"
+          props={exampleProps}
+          setProps={setExampleProps}
+        />
+      </ConfigurableExample>
     </Paper.Group>
   );
 }
--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,6 +1,11 @@
 import "./style.css";
 import * as React from "react";
 
-export default function Spinner(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <span {...props} className="e-spinner" />;
+export interface IProps extends React.HTMLAttributes<HTMLDivElement> {
+  inline?: boolean;
+}
+
+export default function Spinner({ inline, ...rest }: IProps) {
+  const className = `e-spinner${inline ? " e-spinner--inline" : ""}`;
+  return <span {...rest} className={className} />;
 }
