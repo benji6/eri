2b0ae6e876e2e215f3c0f71c88c08dadddadcb81
Ben Hall <benji2357@gmail.com>
2020-08-31 22:00:10 +0000

    fix(Dialog): minor visual improvements

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -45,7 +45,7 @@ export default function Dialog({
           >
             <div className="e-dialog__content">
               <div className="e-dialog__header">
-                <h4 id="e-dialog-title">{title}</h4>
+                <h3 id="e-dialog-title">{title}</h3>
                 <div className="e-dialog__close-button-container">
                   <CloseButton
                     disabled={disableClose || !open}
