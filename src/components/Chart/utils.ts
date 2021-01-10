defa6a7d9f74f97eb5d5cad652ff09caf7f59496
Ben Hall <benji2357@gmail.com>
2021-01-10 16:55:13 +0000

    fix(Chart): a little more breathing room for y axis labels

--- src/components/Chart/utils.ts
+++ src/components/Chart/utils.ts
@@ -2,4 +2,4 @@ export const computeMarginBottom = (xAxisTitle?: string): number =>
   xAxisTitle ? 0.175 : 0.125;
 
 export const computeMarginLeft = (yAxisTitle?: string): number =>
-  yAxisTitle ? 0.175 : 0.125;
+  yAxisTitle ? 0.2 : 0.125;
