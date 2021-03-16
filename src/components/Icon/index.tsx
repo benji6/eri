d49160170ba75f876d7d2cf4ae0dc2c05db1dec8
Ben Hall <benji2357@gmail.com>
2021-03-16 20:17:17 +0000

    feat(Icon): add bell

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,6 +2,12 @@ import "./style.css";
 import * as React from "react";
 
 export const iconNameMap = {
+  bell: (
+    <>
+      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+      <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+    </>
+  ),
   book: (
     <>
       <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20" />
