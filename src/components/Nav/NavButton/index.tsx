fd8ef302302e7576062fbdf474a9c3886dae56a9
Ben Hall <benji2357@gmail.com>
2021-03-25 06:38:53 +0000

    fix(NavButton): make sure it is not visible on high resolutions

--- src/components/Nav/NavButton/index.tsx
+++ src/components/Nav/NavButton/index.tsx
@@ -6,7 +6,7 @@ export default function NavButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) {
   return (
-    <button {...props} className="e-nav-button flex fs-0 fw-b ripple">
+    <button {...props} className="e-nav-button fs-0 fw-b ripple">
       <span className="e-nav-button__text mr-1">Menu</span>
       <Icon name="menu" size="3" />
     </button>
