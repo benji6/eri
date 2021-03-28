341d3519c64e2ccee8bea6bb6e4ed908e37142df
Ben Hall <benji2357@gmail.com>
2021-03-28 13:23:51 +0100

    feat(Icon): add sign-out

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -102,6 +102,13 @@ export const iconNameMap = {
       <line x1="15.41" y1="6.51" x2="8.59" y2="10.49" />
     </>
   ),
+  "sign-out": (
+    <>
+      <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+      <polyline points="16 17 21 12 16 7" />
+      <line x1="21" y1="12" x2="9" y2="12" />
+    </>
+  ),
   sun: (
     <>
       <circle cx="12" cy="12" r="5" />
