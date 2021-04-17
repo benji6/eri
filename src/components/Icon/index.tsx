11fdecba15ece29c45b7ffdd817ec60ebd598f72
Ben Hall <benji2357@gmail.com>
2021-04-17 23:12:09 +0100

    feat(Icon): add pause, play and square icons

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -79,6 +79,13 @@ export const iconNameMap = {
     </>
   ),
   moon: <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />,
+  pause: (
+    <>
+      <rect x="6" y="4" width="4" height="16" />
+      <rect x="14" y="4" width="4" height="16" />
+    </>
+  ),
+  play: <polygon points="5 3 19 12 5 21 5 3" />,
   plus: (
     <>
       <line x1="12" y1="5" x2="12" y2="19" />
@@ -115,6 +122,7 @@ export const iconNameMap = {
       <line x1="21" y1="12" x2="9" y2="12" />
     </>
   ),
+  square: <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />,
   sun: (
     <>
       <circle cx="12" cy="12" r="5" />
