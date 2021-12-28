e77b013384136833acb9f1a86be6147c8b4f0600
Ben Hall <benji2357@gmail.com>
2021-12-28 09:17:08 +0000

    refactor(QuickNav): remove usage of useShouldRenderQuickNav hook

--- src/components/QuickNav/index.tsx
+++ src/components/QuickNav/index.tsx
@@ -5,13 +5,11 @@ import { EriDispatchContext } from "../EriProvider";
 import { PORTAL_CONTAINERS } from "../../constants";
 import QuickNavLink from "./QuickNavLink";
 import useIsWideResolution from "../../hooks/useIsWideResolution";
-import useShouldRenderQuickNav from "../../hooks/useShouldRenderQuickNav";
 
 export default function QuickNav(props: React.HTMLAttributes<HTMLDivElement>) {
   const dispatch = React.useContext(EriDispatchContext);
   const navElRef = React.useRef<HTMLElement>(null);
   const isWideResolution = useIsWideResolution();
-  const shouldRenderQuickNav = useShouldRenderQuickNav();
 
   React.useEffect(() => {
     dispatch({ type: "quickNav/mount" });
@@ -27,8 +25,6 @@ export default function QuickNav(props: React.HTMLAttributes<HTMLDivElement>) {
     });
     return () => dispatch({ payload: 0, type: "quickNav/height" });
   }, [dispatch, isWideResolution, navElRef]);
-
-  if (!shouldRenderQuickNav) return null;
 
   return ReactDOM.createPortal(
     <nav {...props} className="quick-nav bs-1 z-1" ref={navElRef} />,
--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,20 @@
+import "./portals.css";
+
 // The number of highlight colors defined in CSS
 export const HIGHLIGHT_COLOR_COUNT = 6;
 
-function createPortalContainer(): HTMLDivElement | undefined {
+function createPortalContainer(id?: string): HTMLDivElement | undefined {
   if (typeof document === "undefined") return;
-  return document.body.appendChild(document.createElement("div"));
+  const container = document.createElement("div");
+  if (id) container.id = `e-portal-${id}`;
+  return document.body.appendChild(container);
 }
 
 // Order is important here so that things are layered correctly
 /* eslint-disable sort-keys */
 export const PORTAL_CONTAINERS = {
   fab: createPortalContainer(),
-  quickNav: createPortalContainer(),
+  quickNav: createPortalContainer("quick-nav"),
   nav: createPortalContainer(),
   dialog: createPortalContainer(),
 } as const;
