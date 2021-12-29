76f328149abe743deb582c714f955ced44a3df19
Ben Hall <benji2357@gmail.com>
2021-12-29 22:35:57 +0000

    chore(build): fix build
    Fixes 6d04dfb9b0aa4f77e7c2e937ab91baa615f6f8f6

--- scripts/removeCssImports.js
+++ scripts/removeCssImports.js
@@ -1,34 +1,53 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const recursiveMapOverDefinitionFiles = async (f, pathToCheck) => {
-  for (const name of await fs.readdir(pathToCheck)) {
-    const nextPath = path.join(pathToCheck, name);
+const distPath = path.join(__dirname, "..", "dist");
+const esmPath = path.join(__dirname, "..", "esm");
 
-    if ((await fs.lstat(nextPath)).isDirectory()) {
-      recursiveMapOverDefinitionFiles(f, nextPath);
-    } else if (nextPath.endsWith(".d.ts")) {
-      const file = await fs.readFile(nextPath, "utf8");
-      fs.writeFile(nextPath, f(file));
+const recursiveMapOverFilesByExtension =
+  (extension) => (f) => async (pathToCheck) => {
+    for (const name of await fs.readdir(pathToCheck)) {
+      const nextPath = path.join(pathToCheck, name);
+
+      if ((await fs.lstat(nextPath)).isDirectory()) {
+        recursiveMapOverDefinitionFiles(f)(nextPath);
+      } else if (nextPath.endsWith(`.${extension}`)) {
+        const file = await fs.readFile(nextPath, "utf8");
+        fs.writeFile(nextPath, f(file));
+      }
     }
-  }
-};
+  };
 
-const removeCssImports = (definition) =>
+const removeLines = (f) => (definition) =>
   definition
     .split("\n")
-    .filter((line) => {
-      const trimmedLine = line.trim();
-      return !(
-        trimmedLine.startsWith("import") && trimmedLine.endsWith(".css';")
-      );
-    })
+    .filter((line) => !f(line.trim()))
     .join("\n");
 
-const recursiveRemoveCssImports = recursiveMapOverDefinitionFiles.bind(
-  null,
-  removeCssImports
-);
+const recursiveMapOverDefinitionFiles =
+  recursiveMapOverFilesByExtension("d.ts");
 
-recursiveRemoveCssImports(path.join(__dirname, "..", "dist"));
-recursiveRemoveCssImports(path.join(__dirname, "..", "esm"));
+const recursiveRemoveDefinitionCssImports = recursiveMapOverDefinitionFiles(
+  removeLines(
+    (trimmedLine) =>
+      trimmedLine.startsWith("import") && trimmedLine.endsWith('.css";')
+  )
+);
+recursiveRemoveDefinitionCssImports(distPath);
+recursiveRemoveDefinitionCssImports(esmPath);
+
+const recursiveMapOverJsFiles = recursiveMapOverFilesByExtension("js");
+
+recursiveMapOverJsFiles(
+  removeLines(
+    (trimmedLine) =>
+      trimmedLine.startsWith("import") && trimmedLine.endsWith('.css";')
+  )
+)(esmPath);
+
+recursiveMapOverJsFiles(
+  removeLines(
+    (trimmedLine) =>
+      trimmedLine.startsWith('require("') && trimmedLine.endsWith('.css");')
+  )
+)(distPath);
