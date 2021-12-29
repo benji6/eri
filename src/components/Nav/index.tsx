629d336b0e83c26e8369656996296c01927ba767
Ben Hall <benji2357@gmail.com>
2021-12-29 21:34:26 +0000

    refactor(Nav): favor CSS media queries over JS ones

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -19,11 +19,18 @@ interface IProps extends React.HTMLAttributes<HTMLDivElement> {
 export default function Nav({ children, onClose, open, ...rest }: IProps) {
   const isWideResolution = useIsWideResolution();
 
-  return isWideResolution ? (
+  const navEl = (
     /* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
     <nav {...rest} className="nav" onClick={(e) => e.stopPropagation()}>
+      <div className="nav__close-button">
+        <CloseButton onClick={onClose} />
+      </div>
       {children}
     </nav>
+  );
+
+  return isWideResolution ? (
+    navEl
   ) : (
     <Mask onClose={onClose} open={open} portalContainer={PORTAL_CONTAINERS.nav}>
       <CSSTransition
@@ -33,13 +40,7 @@ export default function Nav({ children, onClose, open, ...rest }: IProps) {
         timeout={{ exit: getCssTime1() + 100 }}
         unmountOnExit
       >
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
-        <nav {...rest} className="nav" onClick={(e) => e.stopPropagation()}>
-          <div className="nav__close-button">
-            <CloseButton onClick={onClose} />
-          </div>
-          {children}
-        </nav>
+        {navEl}
       </CSSTransition>
     </Mask>
   );
