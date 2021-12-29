515577dcff8248403163519e25d299a1716a8666
Ben Hall <benji2357@gmail.com>
2021-12-29 21:13:50 +0000

    fix(QuickNav): fix margin bottom on main as window resizes

--- src/components/QuickNav/index.tsx
+++ src/components/QuickNav/index.tsx
@@ -3,30 +3,28 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { PORTAL_CONTAINERS } from "../../constants";
 import QuickNavLink from "./QuickNavLink";
-import useIsWideResolution from "../../hooks/useIsWideResolution";
 
 export default function QuickNav(props: React.HTMLAttributes<HTMLDivElement>) {
   const navElRef = React.useRef<HTMLElement>(null);
-  const isWideResolution = useIsWideResolution();
 
   React.useEffect(() => {
+    const handler = () => {
+      if (!navElRef.current) return;
+      const boundingClientRect = navElRef.current.getBoundingClientRect();
+      document.documentElement.style.setProperty(
+        "--quick-nav-height",
+        `${boundingClientRect.height}px`
+      );
+    };
     document.documentElement.classList.add("quick-nav-mounted");
+    addEventListener("resize", handler);
+    handler();
     return () => {
       document.documentElement.classList.remove("quick-nav-mounted");
       document.documentElement.style.setProperty("--quick-nav-height", "0px");
+      removeEventListener("resize", handler);
     };
   }, []);
-
-  React.useEffect(() => {
-    if (isWideResolution || !navElRef.current) return;
-    const boundingClientRect = navElRef.current.getBoundingClientRect();
-    document.documentElement.style.setProperty(
-      "--quick-nav-height",
-      `${boundingClientRect.height}px`
-    );
-    return () =>
-      document.documentElement.style.setProperty("--quick-nav-height", "0px");
-  }, [isWideResolution, navElRef]);
 
   return ReactDOM.createPortal(
     <nav {...props} className="quick-nav bs-1 z-1" ref={navElRef} />,
