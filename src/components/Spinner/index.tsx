5fdf7f0ebabda88e293c2b8390471081b2cc36ea
Ben Hall <benji2357@gmail.com>
2022-11-05 13:16:09 +0000

    feat(Spinner): add `false` option to `margin` prop

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,12 +3,12 @@ import * as React from "react";
 
 export interface IProps extends React.HTMLAttributes<HTMLDivElement> {
   inline?: boolean;
-  margin?: "start" | "end";
+  margin?: false | "start" | "end";
 }
 
 export default function Spinner({ inline, margin, ...rest }: IProps) {
   const className = `spinner br-max${inline ? " spinner--inline" : ""}${
-    margin ? ` spinner--margin-${margin}` : ""
+    margin === undefined ? "" : ` spinner--margin-${margin}`
   }`;
   return <span {...rest} className={className} />;
 }
