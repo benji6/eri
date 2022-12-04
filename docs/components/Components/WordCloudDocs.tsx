bace423b9532f838424a0c4ccf590db86ac265b4
Ben Hall <benji2357@gmail.com>
2022-12-04 00:15:45 +0000

    docs(WordCloud): more example word clouds for visual testing

--- docs/components/Components/WordCloudDocs.tsx
+++ docs/components/Components/WordCloudDocs.tsx
@@ -18,7 +18,59 @@ export default function WordCloudDocs() {
       <Paper>
         <h2>WordCloud</h2>
       </Paper>
-      <ConfigurableExample example={<WordCloud words={words} />} />
+      <ConfigurableExample
+        example={
+          <WordCloud
+            words={Object.fromEntries(Object.entries(words).slice(0, 150))}
+          />
+        }
+      />
+      <Paper>
+        <h2>{Object.keys(words).length} pokemon</h2>
+        <WordCloud words={words} />
+      </Paper>
+      <Paper>
+        <h2>{Object.keys(words).length} pokemon with narrow weighting range</h2>
+        <WordCloud
+          words={Object.fromEntries(
+            Object.entries(words).map(([k, v]) => [k, v / 64])
+          )}
+        />
+      </Paper>
+      <Paper>
+        <h2>40 pokemon</h2>
+        <WordCloud
+          words={Object.fromEntries(Object.entries(words).slice(0, 40))}
+        />
+      </Paper>
+      <Paper>
+        <h2>20 pokemon</h2>
+        <WordCloud
+          words={Object.fromEntries(Object.entries(words).slice(0, 20))}
+        />
+      </Paper>
+      <Paper>
+        <h2>10 pokemon</h2>
+        <WordCloud
+          words={Object.fromEntries(Object.entries(words).slice(0, 10))}
+        />
+      </Paper>
+      <Paper>
+        <h2>10 pokemon with narrow weighting range</h2>
+        <WordCloud
+          words={Object.fromEntries(
+            Object.entries(words)
+              .slice(0, 10)
+              .map(([k, v]) => [k, v / 64])
+          )}
+        />
+      </Paper>
+      <Paper>
+        <h2>5 pokemon</h2>
+        <WordCloud
+          words={Object.fromEntries(Object.entries(words).slice(0, 5))}
+        />
+      </Paper>
     </Paper.Group>
   );
 }
