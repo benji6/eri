a15ef3ec8fd6f29e4f8817a76fcc12ee4fec367b
Ben Hall <benji2357@gmail.com>
2023-01-15 11:07:52 +0000

    feat(Icon): add at-sign

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,6 +2,12 @@ import "./style.css";
 import * as React from "react";
 
 export const iconNameMap = {
+  "at-sign": (
+    <>
+      <circle cx="12" cy="12" r="4" />
+      <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94" />
+    </>
+  ),
   bell: (
     <>
       <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
