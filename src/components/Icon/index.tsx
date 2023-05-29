7223e474f19451e308a3957142d067c4463c2c36
Ben Hall <benji2357@gmail.com>
2023-05-29 23:37:06 +0300

    feat(Icon): add trash

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -259,6 +259,14 @@ export const iconNameMap = {
   thermometer: (
     <path d="M14 14.76V3.5a2.5 2.5 0 0 0-5 0v11.26a4.5 4.5 0 1 0 5 0z" />
   ),
+  trash: (
+    <>
+      <polyline points="3 6 5 6 21 6" />
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+      <line x1="10" y1="11" x2="10" y2="17" />
+      <line x1="14" y1="11" x2="14" y2="17" />
+    </>
+  ),
   up: <polyline points="18 15 12 9 6 15" />,
   weight: (
     <>
